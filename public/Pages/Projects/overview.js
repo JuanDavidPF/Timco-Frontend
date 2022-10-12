87f9e8253924185f268f79bf24f463f6889b9dc3
Juan Payán <juandavidPF@gmail.com>
2022-10-12 14:06:54 -0500

    Reading data from pokeapi in project overview

---
+++ public/Pages/Projects/overview.js
@@ -0,0 +1,12 @@
+const urlParams = new URLSearchParams(window.location.search);
+const projectKey = urlParams.get('projectId');
+
+
+const ProjectLogo = document.querySelector();
+
+
+const RenderProcectData = (projectKey) => {
+    if (!projectKey) return;
+
+
+}
